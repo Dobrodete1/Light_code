76c80d82dd8164f3dcec762e325989b34f57f9b0
Dobrodete1 <kozho31203@gmail.com>
2022-09-13 12:31:13 +0600

    feat: add user data

--- src/components/Exaple.jsx
+++ src/components/Exaple.jsx
@@ -1,4 +1,52 @@
 import React from 'react';
+const data = [
+    {
+      id: 'user-1',
+      fio: 'Leanne Graham',
+      birthday: '23.06.1998',
+      address: 'Hello world 4/2',
+      phone: '+996 700 866 222',
+      salary: "$3000",
+      favoriteColor: 'red',
+      hobby: 'movie',
+    }, {
+      id: 'user-2',
+      fio: 'Ervin Howell',
+      birthday: '23.06.1998',
+      address: 'Hello world 4/2',
+      phone: '+996 700 866 222',
+      salary: "200$",
+      favoriteColor: 'black',
+      hobby: 'movie',
+    }, {
+      id: 'user-3',
+      fio: 'Clementine Bauch',
+      birthday: '23.06.1998',
+      address: 'Hello world 4/2',
+      phone: '+996 700 866 222',
+      salary: "50000c",
+      favoriteColor: 'brown',
+      hobby: 'movie',
+    }, {
+      id: 'user-4',
+      fio: 'Patricia Lebsack',
+      birthday: '23.06.1998',
+      address: 'Hello world 4/2',
+      phone: '+996 700 866 222',
+      salary: "$1000",
+      favoriteColor: 'gray',
+      hobby: 'movie',
+    }, {
+      id: 'user-5',
+      fio: 'Chelsey Dietrich',
+      birthday: '23.06.1998',
+      address: 'Hello world 4/2',
+      phone: '+996 700 866 222',
+      salary: "1200$",
+      favoriteColor: 'yellow',
+      hobby: 'movie',
+    },
+  ]
 
 const Exaple = () => {
     return (
